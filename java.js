deb9d2777f6d3b6dc9a3d7c48f605cded6a63166
lorenacosta8 <134751881+lorenacosta8@users.noreply.github.com>
2023-08-24 20:20:28 -0400

    Add files via upload

---
+++ java.js
@@ -0,0 +1,10 @@
+const botaoSobrenos = document.getElementById(`sessaoSobrenos`) 
+const sessaoSobreNos = document.getElementById(`sobrenos`)
+
+botaoSobrenos.addEventListener(`click`, qualquerCoisa )
+
+
+
+function qualquerCoisa(){
+    sessaoSobreNos.scrollIntoView({behavior:`smooth`})
+}
